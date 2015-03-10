95571a5d895042036371410b5cc0f532a19b2e94
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-03-10 02:47:45 -0400

    make it candy5

--- candy5.mk
+++ candy5.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/samsung/quincydcm/full_quincydcm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candy5/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common candy5 stuff.
+$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SC-05D TARGET_DEVICE=SC-05D BUILD_FINGERPRINT="samsung/SC-05D/SC-05D:4.1.2/JZO54K/SC05DOMMSI:user/release-keys" PRIVATE_BUILD_DESC="SC-05D-user 4.1.2 JZO54K SC05DOMMSI release-keys"
 
-PRODUCT_NAME := cm_quincydcm
+PRODUCT_NAME := candy5_quincydcm
 PRODUCT_DEVICE := quincydcm
 
